5c6eea0ccea90b60b777c353956cc3d1b7359bea
Íris Barbie Dias Prado <irisbarbiediasprado@gmail.com>
2026-07-19 07:16:28 -0300

    chore: bootstrap Termiris

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+[ -f ~/.termiris/lib/api.sh ] && . ~/.termiris/lib/api.sh
+
+[ -f ~/.termiris/lib/tmux.sh ] && . ~/.termiris/lib/tmux.sh
---
+++ lib/api.sh
@@ -0,0 +1,9 @@
+copiar(){ tmux capture-pane -p > ~/.cache/tmux-capture && termux-clipboard-set < ~/.cache/tmux-capture; }
+colar(){ local cmd; cmd="$(termux-clipboard-get)"; printf "[1;36m>>> %s[0m
+" "$cmd"; builtin eval "$cmd"; }
+leader(){ tmux send-prefix; }
+new_window(){ tmux new-window; }
+split_h(){ tmux split-window -h; }
+split_v(){ tmux split-window -v; }
+next_window(){ tmux next-window; }
+prev_window(){ tmux previous-window; }
